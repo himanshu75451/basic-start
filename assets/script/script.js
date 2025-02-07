380876dbde440c4a315a92a63b0a64d19bce7fa4
himanshu75451 <himanshukalal75451@gmail.com>
2025-02-07 12:13:12 +0530

    Push Code

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+
+// ===================== Common Header/Footer =============================
+
+$(function(){
+    $("#header").load("header.html"); 
+    $("#footer").load("footer.html"); 
+});
+
+
+// function zoom() {
+//     if (window.innerWidth >= 650 && window.innerWidth <= 1400) {
+//         document.body.style.zoom = "90%";
+//     } 
+//     else {
+//         document.body.style.zoom = "100%";
+//     }
+// }
+// window.addEventListener('resize', zoom);
+// zoom();
