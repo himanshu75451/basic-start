0dceefd018483972ec9c65c9d591d92406471639
himanshu75451 <himanshukalal75451@gmail.com>
2025-02-11 10:29:59 +0530

    Changes Done

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,13 +7,40 @@ $(function(){
 });
 
 
-// function zoom() {
-//     if (window.innerWidth >= 650 && window.innerWidth <= 1400) {
-//         document.body.style.zoom = "90%";
-//     } 
-//     else {
-//         document.body.style.zoom = "100%";
-//     }
-// }
-// window.addEventListener('resize', zoom);
-// zoom();
+function zoom() {
+    if (window.innerWidth >= 650 && window.innerWidth <= 1400) {
+        document.body.style.zoom = "90%";
+    } 
+    // else if (window.innerWidth >= 650 && window.innerWidth <= 1300) {
+    //     document.body.style.zoom = "80%";
+    // } 
+    else {
+        document.body.style.zoom = "100%";
+    }
+}
+// Call the zoom function on window resize
+window.addEventListener('resize', zoom);
+// Initial call to set zoom based on current window size
+zoom();
+
+$('.banner-silder').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayHoverPause: false,
+    slideTransition: 'linear',
+    autoplayTimeout: 10000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
